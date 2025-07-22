e66d69c5833eaa097cdf19ea131bab5a410f2818
junyong <rlawnsdyd530@gmail.com>
2025-07-22 20:21:17 +0900

    [REFACTOR][junyong]: 유저 정보 조회 쿼리 1회만 발생 되도록 & ExceptionAdvice 코드 작성

--- src/main/java/kkukmoa/kkukmoa/common/BaseEntity.java
+++ src/main/java/kkukmoa/kkukmoa/common/BaseEntity.java
@@ -1,3 +1,22 @@
 package kkukmoa.kkukmoa.common;
 
-public class BaseEntity {}
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseEntity {
+
+  @CreatedDate
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  private LocalDateTime updatedAt;
+
+}
